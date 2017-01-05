728d173d1c39c3bfe70a35620efa912f68c4058f
Chuck Sakoda <chuck@amperity.com>
2017-01-04 20:44:00 -0800

    Add admin panel

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from app.database import db
+from app.versions.model import Version
+from app.votes.model import Vote
+from app.comments.model import Comment
+from app.bands.model import Band
+from app.songs.model import Song
 
 class base_config(object):
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
@@ -9,4 +16,12 @@ def create_app(config=base_config):
     app = Flask(__name__)
     app.config.from_object(config)
     db.init_app(app)
+
+    admin = Admin(app, name='microblog', template_mode='bootstrap3')
+    admin.add_view(ModelView(Version, db.session))
+    admin.add_view(ModelView(Vote, db.session))
+    admin.add_view(ModelView(Comment, db.session))
+    admin.add_view(ModelView(Band, db.session))
+    admin.add_view(ModelView(Song, db.session))
+
     return app
