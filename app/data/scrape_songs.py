eb1cd667460c9d0f56fc0373db83f0ed4d541ad4
Chuck Sakoda <chuck@amperity.com>
2017-01-04 22:52:35 -0800

    Load song data into DB

--- app/data/scrape_songs.py
+++ app/data/scrape_songs.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-opener = urllib.request.FancyURLopener({})
-url = "http://phish.net/songs"
-f = opener.open(url)
-soup = BeautifulSoup(f)
+def get_song_names():
+    opener = urllib.request.FancyURLopener({})
+    url = "http://phish.net/songs"
+    f = opener.open(url)
+    soup = BeautifulSoup(f)
 
-songs = soup.find_all('tr')[1:]
+    songs = soup.find_all('tr')[1:]
 
-def go():
     names = list(set([s.td.a.text.encode('utf-8') for s in songs]))
     names.sort()
-    for name in names:
-        print(name)
+    return names
--- manage.py
+++ manage.py
@@ -4,10 +4,10 @@ from app.database import db
 from app import create_app
 import app.bands.model
 import app.comments.model
-import app.songs.model
+from app.songs.model import Song
 import app.versions.model
 import app.votes.model
-
+from app.data.scrape_songs import get_song_names
 
 app = create_app()
 migrate = Migrate(app, db)
@@ -15,6 +15,25 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('runserver', Server())
+
+@manager.command
+def load_songdata():
+    """
+    Load songs from phish.net/songs into db
+    """
+
+    meta = db.metadata
+    for table in meta.sorted_tables:
+        if table.name == 'song':
+            print('Clear table %s', table)
+            db.session.execute(table.delete())
+    db.session.commit()
+    for name in get_song_names():
+        s = Song()
+        s.name = name
+        s.band_id = 1
+        db.session.add(s)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
