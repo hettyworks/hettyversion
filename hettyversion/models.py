69623f29c4f68047943493bc023e7242b5b23a9e
Chuck Sakoda <chuck@amperity.com>
2017-01-08 12:15:00 -0800

    Index band_id

--- hettyversion/models.py
+++ hettyversion/models.py
@@ -20,21 +20,21 @@ class Song(db.Model):
     song_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
     desc = db.Column(db.String(128))
-    band_id = db.Column(db.Integer)
+    band_id = db.Column(db.Integer, index=True)
 
 
 class User(db.Model, UserMixin):
-	id = db.Column(db.Integer, primary_key=True)
+        id = db.Column(db.Integer, primary_key=True)
 
-	username = db.Column(db.String(50), nullable=False, unique=True)
-	password = db.Column(db.String(255), nullable=False, server_default='')
+        username = db.Column(db.String(50), nullable=False, unique=True)
+        password = db.Column(db.String(255), nullable=False, server_default='')
 
-	email = db.Column(db.String(255), nullable=False, unique=True)
-	confirmed_at = db.Column(db.DateTime())
+        email = db.Column(db.String(255), nullable=False, unique=True)
+        confirmed_at = db.Column(db.DateTime())
 
-	active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
-	first_name = db.Column(db.String(100), nullable=False, server_default='')
-	last_name = db.Column(db.String(100), nullable=False, server_default='')
+        active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
+        first_name = db.Column(db.String(100), nullable=False, server_default='')
+        last_name = db.Column(db.String(100), nullable=False, server_default='')
 
 
 class Version(db.Model):
