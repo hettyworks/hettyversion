5e3253f8291c193d56b4a8bd67c6679ee4be4af3
Oscar Heller <oheller@gmail.com>
2017-01-08 14:00:40 -0500

    Added custom base.html template, empty style.css, simple nav.

--- hettyversion/views.py
+++ hettyversion/views.py
@@ -5,13 +5,16 @@ from hettyversion.forms import VersionForm
 
 frontend = Blueprint('frontend', __name__)
 
+@frontend.route('/', methods=['GET'])
+def home_page():
+	return render_template('home.html')
+
 @frontend.route('/versions/', methods=['GET', 'POST'])
 def create_version():
     form = VersionForm()
     if form.validate_on_submit():
         return redirect('/success')
     return render_template('new_version.html', form=form)
-
 
 @frontend.route('/members/', methods=['GET'])
 @login_required
