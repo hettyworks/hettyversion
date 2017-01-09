262f9a801463153ee23cd437b203be988efd88ff
Chuck Sakoda <chuck@amperity.com>
2017-01-08 22:40:02 -0800

    Basic vote form

--- hettyversion/forms.py
+++ hettyversion/forms.py
@@ -1,8 +1,21 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, IntegerField
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, DateField, IntegerField, SubmitField, HiddenField, Label
 
 class VersionForm(FlaskForm):
     name = StringField('title')
     date = DateField('date')
     song_id = IntegerField('song_id')
     url = StringField('url')
+
+
+class VoteForm(Form):
+    lhs = SubmitField()
+    rhs = SubmitField()
+    lhs_id = HiddenField()
+    rhs_id = HiddenField()
+
+    def init_candidate(self, lhs, rhs):
+        self.lhs.label = Label(None, lhs.title)
+        self.rhs.label  = Label(None, lhs.title)
+        self.lhs_id.data = lhs.version_id
+        self.rhs_id.data = rhs.version_id
--- hettyversion/views.py
+++ hettyversion/views.py
@@ -1,13 +1,13 @@
 from flask import redirect, Blueprint, render_template
 from flask_user import login_required
-
-from hettyversion.forms import VersionForm
+from hettyversion.forms import VersionForm, VoteForm
+from hettyversion.versions import get_candidate, fight_versions
 
 frontend = Blueprint('frontend', __name__)
 
 @frontend.route('/', methods=['GET'])
 def home_page():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @frontend.route('/versions/', methods=['GET', 'POST'])
 def create_version():
@@ -16,7 +16,20 @@ def create_version():
         return redirect('/success')
     return render_template('new_version.html', form=form)
 
+@frontend.route('/vote/', methods=['GET', 'POST'])
+def present_vote():
+    form = VoteForm()
+    if form.validate_on_submit():
+        if form.lhs.data:
+            return redirect('/lhs-wins')
+        elif form.rhs.data:
+            return redirect('/rhs-wins')
+    else:
+        lhs, rhs = get_candidate()
+        form.init_candidate(lhs, rhs)
+        return render_template('vote.html', form=form)
+
 @frontend.route('/members/', methods=['GET'])
 @login_required
 def members_page():
-	return render_template('members.html')
+    return render_template('members.html')
