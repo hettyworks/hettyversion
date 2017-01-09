652afac092b1d7d4d5eabe6255b8acadca3e18bf
Oscar Heller <oheller@gmail.com>
2017-01-09 15:54:37 -0500

    First test of custom admin views.

--- hettyversion/__init__.py
+++ hettyversion/__init__.py
@@ -11,6 +11,16 @@ from hettyversion.config import base_config
 from hettyversion.views import frontend
 
 
+class SongView(ModelView):
+    column_list = (
+        hettyversion.models.Song.song_id,
+        hettyversion.models.Song.name,
+        hettyversion.models.Song.desc,
+        hettyversion.models.Song.band_id
+    )
+    can_delete = False
+
+
 def create_app(config=base_config):
     app = Flask(__name__)
     app.secret_key = 'some secret key'
@@ -25,7 +35,7 @@ def create_app(config=base_config):
     admin.add_view(ModelView(hettyversion.models.Vote, db.session))
     admin.add_view(ModelView(hettyversion.models.Comment, db.session))
     admin.add_view(ModelView(hettyversion.models.Band, db.session))
-    admin.add_view(ModelView(hettyversion.models.Song, db.session))
+    admin.add_view(SongView(hettyversion.models.Song, db.session))
     admin.add_view(ModelView(hettyversion.models.User, db.session))
 
     db_adapter = SQLAlchemyAdapter(db, hettyversion.models.User)
