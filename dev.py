7c75f4439315dba9b702c3b751e94f2548129d92
Chuck Sakoda <chuck@amperity.com>
2017-01-08 22:14:36 -0800

    Update dev tools

--- dev.py
+++ dev.py
@@ -1,7 +1,9 @@
 import manage
 import importlib
 import hettyversion.versions
-from hettyversion.versions import fight_versions, init_rating, get_version, load_rating
+from hettyversion.database import db
+from hettyversion.versions import fight_versions, init_rating, get_version, load_rating, get_candidate
+from hettyversion.models import Song, Version
 
 def reload():
     importlib.reload(hettyversion.versions)
