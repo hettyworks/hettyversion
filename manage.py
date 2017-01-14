41595f6e5147fa02169e85dc8d313cbd12d03f09
Chuck Sakoda <chuck@amperity.com>
2017-01-14 04:43:28 -0800

    Add manage.py commands for adding roles to users

--- manage.py
+++ manage.py
@@ -2,18 +2,53 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from hettyversion.database import db
 from hettyversion import create_app
-from hettyversion.models import Song, Version, Band
+from hettyversion.models import Song, User, Version, Band, Role
 from hettyversion.data.scrape_songs import get_song_names
 from hettyversion.data.dev import get_song_id, get_song_versions, get_band_id
+from pprint import pprint
 
 app = create_app()
-app.app_context().push() # set default context
+app.app_context().push() # set default context so db.session can find the app
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('runserver', Server())
 
+
+@manager.command
+def list_roles():
+    pprint([vars(r) for r in db.session.query(Role).all()])
+
+@manager.command
+def add_role(role_name):
+    r = Role()
+    r.name = role_name
+    db.session.add(r)
+    db.session.commit()
+
+def dissoc(d, key):
+    d.pop(key, None)
+    return d
+
+def print_model(m):
+    data = vars(m).copy()
+    pprint(dissoc(data, "_sa_instance_state"))
+
+@manager.command
+def list_users(limit=5):
+    for user in db.session.query(User).all():
+        print_model(user)
+        for role in user.roles:
+            print("Roles")
+            print_model(role)
+
+@manager.command
+def grant_role(user_id, role_id):
+    role = db.session.query(Role).filter(Role.id==int(role_id)).one()
+    user = db.session.query(User).filter(User.id==int(user_id)).one()
+    user.roles.append(role)
+    db.session.commit()
 
 @manager.command
 def load_demo():
@@ -67,7 +102,7 @@ def load_bands():
 @manager.command
 def load_hoodvers():
     # Get Harry Hood song_id
-    
+
     meta = db.metadata
     for table in meta.sorted_tables:
         if table.name == 'version':
