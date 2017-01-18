65f696cb7b0c26aa6afeee20ab88a6bc9fa44229
Oscar Heller <oheller@gmail.com>
2017-01-17 23:20:53 -0500

    Merged heads.

---
+++ migrations/versions/a392ca901b70_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a392ca901b70
+Revises: bf8cfb6e1a4b, 20788a590941
+Create Date: 2017-01-17 23:20:39.566899
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a392ca901b70'
+down_revision = ('bf8cfb6e1a4b', '20788a590941')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
