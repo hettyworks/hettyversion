e51a857d32fda1316232f22bb29d3a576fa6e327
Chuck Sakoda <chuck@amperity.com>
2017-01-19 02:56:01 -0800

    Set main port to 5000

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from hettyversion import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=5000)
