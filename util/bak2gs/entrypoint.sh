6f8844aacf2f699b5341a8a3481334d68aa4642d
Chuck Sakoda <chuck@amperity.com>
2017-01-21 02:36:26 -0800

    Make bak2gs loop forever.  Deploy to kube

--- util/bak2gs/entrypoint.sh
+++ util/bak2gs/entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-FILENAME=hettyversion_db_$(date +"%s").sql
-
 cd /tmp
+while [ 1 ]
+do
+    FILENAME=hettyversion_db_$(date +"%s").sql
+    echo "Dumping mysql data from $MYSQL_HOST/$DUMP_DB"
+    mysqldump -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD --databases $DUMP_DB > $FILENAME
+    tar cfvz $FILENAME.tar.gz $FILENAME
 
-echo "Dumping mysql data from $MYSQL_HOST/$DUMP_DB"
-mysqldump -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD --databases $DUMP_DB > $FILENAME
-tar cfvz $FILENAME.tar.gz $FILENAME
+    echo "Uploading to $BAK_BUCKET"
+    gcloud auth activate-service-account --account=316443988803 --key-file=/opt/gs/creds.json
+    gsutil cp $FILENAME.tar.gz $BAK_BUCKET
 
-echo "Uploading to $BAK_BUCKET"
-gcloud auth activate-service-account --key-file=/opt/hettyversion/creds.json
-gsutil cp $FILENAME.tar.gz $BAK_BUCKET
+    rm $FILENAME
+    rm $FILENAME.tar.gz
+
+    echo "Sleeping 1 day"
+    sleep '1d'
+done
